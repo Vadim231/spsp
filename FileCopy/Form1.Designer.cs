a683ab29fe64bf7bc2607c479f890aced80f47b4
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2019-04-04 16:57:33 +0600

    Add files via upload

---
+++ FileCopy/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.txtDestFile = new System.Windows.Forms.TextBox();
+            this.btnSourceFile = new System.Windows.Forms.Button();
+            this.btnDestFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbFileCopy = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(80, 17);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(382, 20);
+            this.txtSourceFile.TabIndex = 0;
+            // 
+            // txtDestFile
+            // 
+            this.txtDestFile.Location = new System.Drawing.Point(80, 43);
+            this.txtDestFile.Name = "txtDestFile";
+            this.txtDestFile.Size = new System.Drawing.Size(382, 20);
+            this.txtDestFile.TabIndex = 1;
+            // 
+            // btnSourceFile
+            // 
+            this.btnSourceFile.Location = new System.Drawing.Point(468, 17);
+            this.btnSourceFile.Name = "btnSourceFile";
+            this.btnSourceFile.Size = new System.Drawing.Size(75, 20);
+            this.btnSourceFile.TabIndex = 2;
+            this.btnSourceFile.Text = "Browse";
+            this.btnSourceFile.UseVisualStyleBackColor = true;
+            this.btnSourceFile.Click += new System.EventHandler(this.btnSourceFile_Click);
+            // 
+            // btnDestFile
+            // 
+            this.btnDestFile.Location = new System.Drawing.Point(468, 43);
+            this.btnDestFile.Name = "btnDestFile";
+            this.btnDestFile.Size = new System.Drawing.Size(75, 20);
+            this.btnDestFile.TabIndex = 3;
+            this.btnDestFile.Text = "Browse";
+            this.btnDestFile.UseVisualStyleBackColor = true;
+            this.btnDestFile.Click += new System.EventHandler(this.btnDestFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Destination";
+            // 
+            // pbFileCopy
+            // 
+            this.pbFileCopy.Location = new System.Drawing.Point(12, 90);
+            this.pbFileCopy.Name = "pbFileCopy";
+            this.pbFileCopy.Size = new System.Drawing.Size(531, 23);
+            this.pbFileCopy.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(15, 138);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStop.Location = new System.Drawing.Point(108, 138);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "&Pause";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(468, 138);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Progress";
+            // 
+            // btnResume
+            // 
+            this.btnResume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnResume.Location = new System.Drawing.Point(201, 138);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 11;
+            this.btnResume.Text = "&Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 178);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbFileCopy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDestFile);
+            this.Controls.Add(this.btnSourceFile);
+            this.Controls.Add(this.txtDestFile);
+            this.Controls.Add(this.txtSourceFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtSourceFile;
+    private System.Windows.Forms.TextBox txtDestFile;
+    private System.Windows.Forms.Button btnSourceFile;
+    private System.Windows.Forms.Button btnDestFile;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.ProgressBar pbFileCopy;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnStop;
+    private System.Windows.Forms.Button btnClose;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button btnResume;
+  }
+}
+
---
+++ FileCopy/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+      btnResume.Enabled = false;
+    }
+
+    private void btnSourceFile_Click(object sender, EventArgs e)
+    {
+      OpenFileDialog dlg = new OpenFileDialog();
+      dlg.CheckFileExists = true;
+      dlg.Multiselect = false;
+      dlg.Title = "Открытие файла";
+      if (dlg.ShowDialog() == DialogResult.OK)
+      {
+        txtSourceFile.Text = dlg.FileName;
+      }
+    }
+    private void btnDestFile_Click(object sender, EventArgs e)
+    {
+      SaveFileDialog dlg = new SaveFileDialog();
+      dlg.CheckFileExists = false;
+      dlg.OverwritePrompt = true;
+      if (dlg.ShowDialog() == DialogResult.OK)
+      {
+        txtDestFile.Text = dlg.FileName;
+      }
+      else txtDestFile.Text = "";
+    }
+
+    Thread thCopyFile = null;
+    CopyParam paramCopyFile = null;
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      if (txtSourceFile.Text.Trim().Length == 0 ||
+         txtDestFile.Text.Trim().Length == 0)
+      {
+        MessageBox.Show("Файл не указан");
+        return;
+      }
+      if (thCopyFile != null)
+      {
+        return;
+      }
+      thCopyFile = new Thread(ThCopyRoutine);
+      thCopyFile.IsBackground = true;
+      paramCopyFile = new CopyParam();
+
+      paramCopyFile.srcFileName =
+                      txtSourceFile.Text.Trim();
+      paramCopyFile.destFileName =
+                      txtDestFile.Text.Trim();
+
+      paramCopyFile.frm = this;
+      pbFileCopy.Value = 0;
+      pbFileCopy.Minimum = 0; 
+      pbFileCopy.Maximum = 1000; 
+      pbFileCopy.Step = 100; 
+
+      thCopyFile.Start(paramCopyFile);
+    }
+
+    int readSize = 2*1024; 
+
+    void ThCopyRoutine(object arg)
+    {
+      CopyParam par = arg as CopyParam;
+      FileStream src =
+        new FileStream(par.srcFileName,
+                       FileMode.Open,
+                       FileAccess.Read);
+      FileStream dst = 
+        new FileStream(par.destFileName,
+                       FileMode.OpenOrCreate,
+                       FileAccess.Write);
+
+      byte[] buf = new byte[readSize];
+            
+
+      FileInfo fi = new FileInfo(par.srcFileName);
+      long fileSize = fi.Length;
+      long readAll = 0;
+      while (!par.IsStop)
+      {
+        int readBytes = src.Read(buf, 0, readSize);
+        dst.Write(buf, 0, readBytes);
+
+        readAll += readBytes;
+
+        int readProcent =
+    (int)((double)readAll / fileSize * 100.0 * 10 + 0.5);
+        par.frm.pbFileCopy.Invoke(new Action<int>(
+          (x) => { par.frm.pbFileCopy.Value = x;
+            par.frm.pbFileCopy.Update();
+          }),
+          readProcent);
+                
+        if(readAll == fileSize) {
+          par.IsStop = true;
+        }
+        par.evnPause.WaitOne();
+      }
+    }
+
+    private void btnStop_Click(object sender, EventArgs e)
+    {
+            Stop();
+    }
+
+        void Stop()
+        {
+            btnResume.Enabled = true;
+            paramCopyFile.evnPause.Reset();
+            paramCopyFile.IsStop = true;
+        }
+
+    private void btnResume_Click(object sender, EventArgs e)
+    {
+            Resume();
+    }
+
+        void Resume()
+        {
+            paramCopyFile.evnPause.Set();
+            paramCopyFile.IsStop = false;
+            btnResume.Enabled = false;
+        }
+
+    private void btnClose_Click(object sender, EventArgs e)
+    {
+            if (paramCopyFile == null) { Application.Exit();return; }
+            Stop();
+            var result = MessageBox.Show("Вы уверены, что хотите прекратить работу программы?", "Упс", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                Resume();
+            }
+    }
+  }
+}
